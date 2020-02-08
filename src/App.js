aac37dcdd50ebd9ecb945c779323d33d38a15180
Vedran <473826+vedran@users.noreply.github.com>
2020-02-07 20:47:52 -0800

    temp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import TaskCard from "./TaskCard";
+import { styled } from "styled-components";
 
-import openSocket from 'socket.io-client'
-const socket = openSocket('http://localhost:4001')
+const TODOS = [
+  {
+    context: "sitrep",
+    title: "Frontend render list of TODOs. Simple cards with Styled components."
+  },
+  {
+    context: "sitrep",
+    title: "Frontend render simple blank calendar layout"
+  },
+  {
+    context: "sitrep",
+    title: "Create hardcoded time buckets to start"
+  },
+  {
+    context: "sitrep",
+    title: "Frontend render time block blockets"
+  },
+  {
+    context: "sitrep",
+    title: "Frontend drag and drop skeleton"
+  },
+  {
+    context: "sitrep",
+    title: "Frontend redux action for drag and drop with redux toolkit"
+  },
+  {
+    context: "sitrep",
+    title: "Frontend UX for checking off items"
+  },
+  {
+    context: "sitrep",
+    title: "Frontend page for editing raw TODOs"
+  },
+  {
+    context: "sitrep",
+    title: "Redux actions for checking off items"
+  }
+];
 
 function App() {
-  const [time, setTime] = useState(new Date())
-  useEffect(() => {
-    socket.on("clock", newTime => {
-      setTime(new Date(newTime));
-    });
-  }, []);
-   
-
   return (
     <div>
-      {time.toISOString()}
+      {TODOS.map(({ context, title }, i) => (
+        <div
+          css={css`
+            color: red;
+          `}
+        >
+          <TaskCard key={i} context={context} title={title} />
+        </div>
+      ))}
     </div>
   );
 }
